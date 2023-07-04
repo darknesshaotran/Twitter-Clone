71b480c20ecd57d4517b9ad79ebb45ed3d594f21
darknesshaotran <darknessclmm@gmail.com>
2023-07-04 15:54:52 +0700

    add refresh token and access token

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,16 +1,49 @@
 import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
+import { registerReqBody } from '~/models/requests/User.requests'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enums'
 
 class UsersService {
-  async register(payload: { email: string; password: string }) {
-    const { email, password } = payload
+  private signAccessToken = (userId: string) => {
+    return signToken({
+      payload: {
+        userId: userId,
+        type: TokenType.AccessToken
+      },
+      options: {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+  private signRefreshToken = (userId: string) => {
+    return signToken({
+      payload: {
+        userId: userId,
+        type: TokenType.RefreshToken
+      },
+      options: {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+
+  ///////////// REGISTER FUNCTIONS //////////////
+  async register(payload: registerReqBody) {
     const result = await databaseService.users.insertOne(
       new User({
-        email: email,
-        password: password
+        ...payload,
+        date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password)
       })
     )
-    return result
+    const userID = result.insertedId.toString()
+    const [AccessToken, RefreshToken] = await Promise.all([this.signAccessToken(userID), this.signRefreshToken(userID)])
+    return {
+      AccessToken,
+      RefreshToken
+    }
   }
 
   async checkExistedEmail(email: string) {
